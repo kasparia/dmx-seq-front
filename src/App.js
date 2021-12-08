f7f6e77395762be820c7210a57374c53b3976024
Kasperi <kasperi@Kasperis-MacBook-Pro.local>
2021-12-08 23:38:39 +0200

    Add initial step sequencer steps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import DMXConnect from './helpers/dmxConnect';
+import styled from 'styled-components';
+
+const StepGrid = styled.div`
+  & {
+    display: flex;
+    margin: 20px 0;
+  }
+
+  input {
+    margin: 0 10px;
+    box-shadow: 0px 8px #ffffcc;
+  }
+
+  input[type="checkbox"]:selected {
+    box-shadow: 0px 8px #00ffcc;
+  }
+`;
 
 function App() {
 
   const connector = new DMXConnect();
-  connector.setFlashRate(255);
+  //connector.setFlashRate(255);
+
+  const setStep = (event) => {
+    const index = parseInt(event.target.id.slice(-1)) - 1;
+    const value = event.target.checked;
+    connector.setStepSequencer(index, value);
+    
+    // console.log(event.target.checked);
+  }
 
   return (
     <div className='App'>
@@ -14,13 +39,20 @@ function App() {
           <input
             type='range'
             min='10'
-            max='1000'
-            defaultValue='250'
+            max='150'
+            defaultValue='60'
             classNmae='slider'
             id='rateSlider'
             onChange={ (event) => connector.setFlashRate(event.target.value) }  
           />
         </div>
+
+        <StepGrid>
+          <input type='checkbox' id='step_1' name='step_1' onChange={ (event) => setStep(event) } />
+          <input type='checkbox' id='step_2' name='step_2' onChange={ (event) => setStep(event) } />
+          <input type='checkbox' id='step_3' name='step_3' onChange={ (event) => setStep(event) } />
+          <input type='checkbox' id='step_4' name='step_4' onChange={ (event) => setStep(event) } />
+        </StepGrid>
       </header>
     </div>
   );
--- src/helpers/dmxConnect.js
+++ src/helpers/dmxConnect.js
@@ -1,3 +1,5 @@
+// import { io } from 'socket.io';
+
 class DMXConnect {
   constructor () {
     this.baselURL = '/api';
@@ -5,16 +7,40 @@ class DMXConnect {
 
     this.throttleTime = 500;
     this.requestTime = 0;
+
+    this.currentBPM = 60;
+    //this.lastBPMCountTime = 0;
+    this.bpmInterval = 0;
+
+    this.flashRate = 0;
+
+
+    this.sender = null;
+
+    this.stepSequencer = [
+      false,
+      false,
+      false,
+      false
+    ];
+
   }
 
   setFlashRate (rate) {
-    console.log(rate);
-    this.sendRequest({
-      flashRate: rate
-    });
+    this.flashRate = rate;
+    this.sendRequest();
   }
 
-  sendRequest (requestBody) {
+  setStepSequencer (stepIndex, stepValue) {
+    this.stepSequencer[stepIndex] = stepValue;
+    this.sendRequest();
+  }
+
+  getStepSequencer () {
+    return this.stepSequencer;
+  }
+
+  sendRequest (rate) {
     const requestTimeNow = Date.now();
     
     // if (this.requestTime === 0 || this.requestTime + this.throttleTime < requestTimeNow) {
@@ -27,14 +53,16 @@ class DMXConnect {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(requestBody)
+          body: JSON.stringify({
+            currentBPM: this.currentBPM,
+            steps: this.getStepSequencer()
+          })
         });
         const content = await rawResponse.json();
       
         console.log(content);
       })();
-    }
-  // }
+  }
 }
 
 export default DMXConnect;
